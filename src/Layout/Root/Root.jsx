12932ebae51a9b09b3d3125648c3c32080fb314f
Akash <shoaibsarkarakash019@gmail.com>
2023-12-21 21:53:31 +0600

    Add SignUp page

--- src/Layout/Root/Root.jsx
+++ src/Layout/Root/Root.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../Shared/Navber/Navber";
+import Footer from "../Shared/Footer/Footer";
 
 
 const Root = () => {
@@ -10,6 +11,7 @@ const Root = () => {
       <Navbar></Navbar>
 
            <Outlet></Outlet> 
+           <Footer></Footer>
         </div>
     );
 };
